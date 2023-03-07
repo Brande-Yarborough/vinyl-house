8cdd953b88f28aa6a2ab79fb9344da257c286d46
BrandeYarborough <brandeyarborough@gmail.com>
2023-03-06 20:11:43 -0500

    adds albums to admin py

--- albums/admin.py
+++ albums/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import AlbumDetail, Album, Comment
 
 # Register your models here.
+admin.site.register(AlbumDetail)
+admin.site.register(Album)
+admin.site.register(Comment)
