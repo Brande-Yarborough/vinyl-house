8de996c16e6be8cddb854f5953b24a5e4d6f71ed
BrandeYarborough <brandeyarborough@gmail.com>
2023-03-08 21:28:23 -0500

    updates registration form and styling

--- frontend/static/src/Pages/RegistrationForm.js
+++ frontend/static/src/Pages/RegistrationForm.js
@@ -49,65 +49,67 @@ const Register = () => {
 
   return (
     <>
-      <Container>
-        <Form onSubmit={handleSubmit}>
-          <Form.Group className="mb-3" controlId="formBasicUsername">
-            <Form.Label>Username</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Enter username"
-              name="username"
-              value={state.username}
-              onChange={handleInput}
-            />
-          </Form.Group>
+      <div id="background-image">
+        <Container id="register-container">
+          <Form id="register" onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicUsername">
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter username"
+                name="username"
+                value={state.username}
+                onChange={handleInput}
+              />
+            </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword1">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Enter password"
-              name="password1"
-              value={state.password1}
-              onChange={handleInput}
-              required
-            />
-          </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword1">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Enter password"
+                name="password1"
+                value={state.password1}
+                onChange={handleInput}
+                required
+              />
+            </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword2">
-            <Form.Label>Verify Password</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Verify password"
-              name="password2"
-              value={state.password2}
-              onChange={handleInput}
-              required
-            />
-            {!passwordMatch && (
-              <Form.Text className="text-danger">
-                Passwords do not match
-              </Form.Text>
-            )}
-          </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword2">
+              <Form.Label>Verify Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Verify password"
+                name="password2"
+                value={state.password2}
+                onChange={handleInput}
+                required
+              />
+              {!passwordMatch && (
+                <Form.Text className="text-danger">
+                  Passwords do not match
+                </Form.Text>
+              )}
+            </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email Address</Form.Label>
-            <Form.Control
-              //   id="email"
-              type="email"
-              placeholder="Enter email"
-              name="email"
-              value={state.email}
-              onChange={handleInput}
-            />
-          </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control
+                //   id="email"
+                type="email"
+                placeholder="Enter email"
+                name="email"
+                value={state.email}
+                onChange={handleInput}
+              />
+            </Form.Group>
 
-          <Button className="register-button" variant="primary" type="submit">
-            Register
-          </Button>
-        </Form>
-      </Container>
+            <Button className="register-button" variant="primary" type="submit">
+              Register
+            </Button>
+          </Form>
+        </Container>
+      </div>
     </>
   );
 };
