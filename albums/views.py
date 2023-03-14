623ebb70463f5f0cda1e1d75181955a8a5d262af
BrandeYarborough <brandeyarborough@gmail.com>
2023-03-14 15:45:17 -0400

    updates function based view for search albums to only return page one of results

--- albums/views.py
+++ albums/views.py
@@ -19,7 +19,7 @@ d = discogs_client.Client('VinylHouse/0.1',
 @api_view(['GET'])
 def search_album(request, query):
     # search for albums matching given query using the 'search' function from the 'd' module, and filter by 'release' type
-    results = d.search(query, type='release')
+    results = d.search(query, type='release').page(1)
     # create empty list to store details of matching releases
     releases = []
     # loop through each result from search
